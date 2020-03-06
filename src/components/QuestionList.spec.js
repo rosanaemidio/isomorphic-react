97212e81c7070fd9d21a89377e12a6d4e1ac174b
rosanaemidio <rosanaemidio42@gmail.com>
2020-03-06 16:46:46 -0300

    Add test's in QuestionList and App

---
+++ src/components/QuestionList.spec.js
@@ -0,0 +1,21 @@
+describe('The Question List', ()=>{
+    beforeEach(()=>{
+        console.log("Before Each!")       
+    })
+    beforeAll(()=>{
+        console.log("Before All");
+        
+    })
+    afterEach(()=>{
+        console.log("after each");        
+    })
+    afterAll(()=>{
+        console.log("after All");        
+    })
+    it.only("should display a list of items", ()=>{
+        expect(40+2).toEqual(42)
+    })
+    it("should display a list of lovers", ()=>{
+        expect(40+2).toEqual(42)
+    })
+})
---
+++ src/components/__tests__/QuestionDetail.js
@@ -0,0 +1,9 @@
+
+describe('The Question Detail component', ()=>{
+    beforeEach(() =>{
+        console.log("Before Each");       
+    })
+    it('should not regress', ()=>{
+        expect(40 + 2).toEqual(42);
+    })
+})
