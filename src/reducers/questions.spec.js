770264cccdc35b55342d1b9fe9b5fd2ca93fbda8
rosanaemidio <rosanaemidio42@gmail.com>
2020-03-11 11:44:15 -0300

    Add tests with matchers

--- src/reducers/questions.spec.js
+++ src/reducers/questions.spec.js
@@ -8,5 +8,16 @@ describe("THe question reducer", ()=>{
        
        expect(newState).toEqual(stateClone);
        expect(newState).toBe(state);
+    });
+    it("should add new question", ()=>{
+        const state = [{question_id:"foo"},{question_id:"bar"}];
+        const newQuestion = {question_id:"baz"};
+        const newState = questions(state,{type:"FETCHED_QUESTION", question:newQuestion});
+
+        console.log(newState);
+
+        expect(newState).toContain(newQuestion);
+        expect(state).not.toContain(newQuestion);
+        expect(newState).toHaveLength(3); 
     })
 })
